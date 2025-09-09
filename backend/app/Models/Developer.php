7cfa112cc9e365c15f4daaf66d7dc61e1e55a846
Rodrigo Serafim <rdgo.serafim@gmail.com>
2025-09-09 15:34:56 -0300

    adiciona modelos Developer e Level com migrações correspondentes

---
+++ backend/app/Models/Developer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Developer extends Model
+{
+    protected $fillable = ['name', 'sexo', 'data_nascimento', 'hobby', 'level_id'];
+
+    protected $casts = [
+        'data_nascimento' => 'date',
+    ];
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
+}
